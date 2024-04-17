e9efee7f7a67efbe2d4f407fd7ff8b74b2fcfb60
Sandeep Vangipuram <sandeep84@gmail.com>
2024-04-17 21:11:24 +0100

    Initial commit

---
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -0,0 +1,14 @@
+-- define common options
+local opts = {
+    noremap = true,      -- non-recursive
+    silent = true,       -- do not show message
+}
+
+-----------------
+-- Normal mode --
+-----------------
+
+-- Hint: see `:h vim.map.set()`
+-- Better window navigation
+vim.keymap.set('n', '<F5>', ':NvimTreeToggle<CR>', opts)
+
